99f3ba5554361d9e447384aeacd833ab32fe2fa1
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-03-10 22:49:25 +0900

    for good job

--- test.js
+++ test.js
@@ -1,16 +1,45 @@
-let twoSum=function(nums,target){
-    let map={};
-    for(i=0;i<nums.length;i++){
-        let diff=target-nums[i];
-        if(map.hasOwnProperty(diff)){
-            return [map[diff],i];
-        }
-        map[nums[i]]=i;
+// var removeDuplicates = function(nums) {
+//     let array=[];
+//     nums.forEach(element=>{
+//         if(!(array.includes(element))){
+//             array.push(element);
+//         }
+        
+//     })
+//     return array.length;
+// };
+
+// var removeDuplicates = function(nums) {
+//     let single_number=nums.length;
+    
+//     for(i=0;i<nums.length;i++){        
+//         for(j=i+1;j<nums.length;j++){
+//             if(nums[i]==nums[j]){
+//                 single_number--;
+//             }
+            
+//         }
+//     }
+//     return single_number;
+// };
+
+
+// 非常 反智
+var removeDuplicates = function(nums) {
+     if(nums.length==0){return 0;}
+	let i = 0;
+
+	for(let j=1; j < nums.length; j++){
+		if(nums[i] !== nums[j]){
+            //如果下一个不等于第一个就加一，然后把不等于的第一个变成下一个。
+			i++;
+			nums[i] = nums[j];
+		}
     }
-    return null;
-}
+    console.log(nums);   //[ 1, 2, 2 ]
+	return i+1;
+};
 
+nums = [1,1,2];
+console.log(removeDuplicates(nums));
 
-nums=[3,3];
-target=6;
-console.log(twoSum(nums,target));
